811acaf7c1d7b97ac1d214cd1b44e4b4bd961af9
Higor Chagas <higornchagas@gmail.com>
2025-04-22 17:53:50 -0300

    feat: add logic to rotate the ship left and right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,8 +9,13 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -18,7 +24,11 @@ def main():
             
         
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.update()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
