c8ab57316042beb6636a7f5320be173469dee1ab
Higor Chagas <higornchagas@gmail.com>
2026-01-24 21:40:18 -0300

    feat(render): implement a triangle drawing in the center of the screen.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
+
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -12,9 +23,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
